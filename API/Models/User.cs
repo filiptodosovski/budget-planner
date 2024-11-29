5a91b48b2d86204a08e81c35ce445bd3dcc686ba
filiptodosovski <filiptodosovski@gmail.com>
2024-11-29 14:46:10 +0100

    feat: connected to postgres database and added User model

---
+++ API/Models/User.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Models;
+
+public class User 
+{
+    public int Id { get; set; }
+    
+    public required string UserName { get; set; }
+    
+    public required string Email { get; set; }
+    
+    public required string Password { get; set; }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,17 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
